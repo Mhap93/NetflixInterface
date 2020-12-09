726eb8270ffdd42f4814732becdfaca84d439076
Marcel Pedroso <marcelhenripedroso@hotmail.com>
2020-12-09 04:55:33 -0300

    Netflix Interface

---
+++ Desafio1/desafio1/desafioextra.js
@@ -0,0 +1,7 @@
+function assistiragora(){
+  window.open("https://www.youtube.com/watch?v=2wqzcLyaU8c");
+}
+
+function information(){
+  window.open("https://www.themoviedb.org/tv/1416-grey-s-anatomy");
+}
---
+++ EstudoFlix/desafio2/estudoflix.js
@@ -0,0 +1,31 @@
+function iniciaragora(){
+  window.open("https://digitalinnovation.one/sign-up?ref=5NT0U9SJNO");
+}
+
+function information(){
+  window.open("https://www.devmedia.com.br/quem-quer-ser-um-programador-fullstack/38786");
+}
+
+function dio(){
+  window.location.href="https://digitalinnovation.one/sign-up?ref=5NT0U9SJNO";
+}
+
+function curso(){
+  window.location.href="https://www.youtube.com/user/cursosemvideo";
+}
+
+function rocket(){
+  window.location.href="https://rocketseat.com.br/";
+}
+
+function filipe(){
+  window.location.href="https://www.youtube.com/channel/UCU5JicSrEM5A63jkJ2QvGYw";
+}
+
+function eu(){
+  window.location.href="https://eucapacito.com.br/cursos/";
+}
+
+function digital(){
+  window.location.href="https://www.digitalhouse.com/br/bolsas/estacao-hack";
+}
